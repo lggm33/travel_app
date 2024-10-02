9dec4865cea5aa0e04173483753568c5e34da653
lggm <lgomez@twilio.com>
2024-10-02 16:12:26 -0600

    test to fix images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = "process.env.NODE_ENV" === 'production';
+const isProd = process.env.NODE_ENV === 'production';
 
-console.log("process.env.NODE_ENV:" + process.env.NODE_ENV)
+console.log({isProd})
 
 const nextConfig = {
   basePath: isProd ? '/travel_app' : '',
