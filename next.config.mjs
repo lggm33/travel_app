45cfbfeab038787de5f30edf91fa3c69b08fbca7
lggm <lgomez@twilio.com>
2024-10-02 15:53:21 -0600

    base path added to next config to fix images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  basePath: isProd ? '/travel_app' : '',
+  assetPrefix: isProd ? '/travel_app/' : '',
+};
 
 export default nextConfig;
